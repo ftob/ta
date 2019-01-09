498a6788b3e251f0377d1818038407c8607b6325
Nikita Volkov <litebackend@gmail.com>
2019-01-09 17:15:57 +0700

    Cosmetics

--- main.go
+++ main.go
@@ -15,14 +15,14 @@ import (
 )
 
 const (
-	defaultPort              = "8080"
+	defaultPort = "8080"
 )
 
 func main() {
 
 	var (
-		addr   = envString("PORT", defaultPort)
-		httpAddr          = flag.String("http.addr", ":"+addr, "HTTP listen address")
+		addr     = envString("PORT", defaultPort)
+		httpAddr = flag.String("http.addr", ":"+addr, "HTTP listen address")
 	)
 
 	flag.Parse()
@@ -30,7 +30,6 @@ func main() {
 	var logger log.Logger
 	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
-
 
 	fieldKeys := []string{"method"}
 
@@ -40,25 +39,24 @@ func main() {
 	ix = index.NewInstrumentingService(
 		kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
 			Namespace: "api",
-			Subsystem: "booking_service",
+			Subsystem: "index_service",
 			Name:      "request_count",
 			Help:      "Number of requests received.",
 		}, fieldKeys),
 		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
 			Namespace: "api",
-			Subsystem: "booking_service",
+			Subsystem: "index_service",
 			Name:      "request_latency_microseconds",
 			Help:      "Total duration of requests in microseconds.",
 		}, fieldKeys),
 		ix,
 	)
 
-
 	srv := server.New(ix, log.With(logger, "component", "http"))
 
 	errs := make(chan error, 2)
 	go func() {
-		logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
+		_ = logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
 		errs <- http.ListenAndServe(*httpAddr, srv)
 	}()
 	go func() {
@@ -67,7 +65,7 @@ func main() {
 		errs <- fmt.Errorf("%s", <-c)
 	}()
 
-	logger.Log("terminated", <-errs)
+	_ = logger.Log("terminated", <-errs)
 }
 
 func envString(env, fallback string) string {
